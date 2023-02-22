31d1d97e89d09cfde4d99775a12e9f0835f2315f
lololebg <111436011+lololebg@users.noreply.github.com>
2023-02-22 20:19:46 +0100

    Update Navbar.jsx

--- CHEENNE_Nathan_Kasa_300123/src/components/navbar/Navbar.jsx
+++ CHEENNE_Nathan_Kasa_300123/src/components/navbar/Navbar.jsx
@@ -1,16 +1,13 @@
 import './navbar.scss'
 import { Link } from 'react-router-dom'
-// Function react qui retourne un composant de barre de navigation.
+
 export default function Navbar() {
-// Utilisation de window.location.pathnanme pour récuper l'emplacement actuel de l'url.
     const currentRoute = window.location.pathname;
 
     return (
-// Class CSS
-// link est un composant de react router, il a un propriété qui définit la destination de chaque lien de navigation.
-    <nav className='NavBar'>
+     <nav className='NavBar'>
         <ul className='Nav'>
-        <li className={currentRoute === '/Kasa/' ? 'Nav_item_active' : 'Nav_item'}>
+        <li className={currentRoute === '/*' ? 'Nav_item_active' : 'Nav_item'}>
             <Link  to='/Kasa/'>
                 Accueil
             </Link>
